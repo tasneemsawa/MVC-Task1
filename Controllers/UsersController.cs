0701636f08279708e1dc1782fe96627ddc8ab126
Tasneem Sawalha <tasneem31997@gmail.com>
2026-05-16 22:13:34 +0300

    update/delete

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -10,21 +10,58 @@ namespace WebApplication1.Controllers
         ApplicationDbContext context = new ApplicationDbContext();
         public ViewResult Index()
         {
-            var user= context.Users2.ToList();
-            return View("Index", user);
-
+            var products = context.Users2.ToList();
+            return View("Index", products);
         }
-
+        //Create
         public ViewResult Create()
         {
-            return View("Create");
+            return View("Create", new User2());
         }
 
-        public ViewResult Store(User2 Request)
+        public IActionResult Store(User2 request)
         {
-            context.Users2.Add(Request);
-            context.SaveChanges();
-            return View("Create");
+            if (ModelState.IsValid)
+            {
+                context.Users2.Add(request);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View("Create", request);
+           
         }
+
+        //Details
+        public IActionResult Details(int id)
+        { 
+           var user = context.Users2.Find(id);
+            return View("Details", user);
+        }
+
+        //Edit button
+        public IActionResult Edit(int id)
+        {
+            var user = context.Users2.Find(id);
+            return View("Edit", user);
+        }
+
+        //Uodate
+        public IActionResult Update(User2 request, int id)
+        {
+            request.Id = id;
+            context.Users2.Update(request);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        //Delete
+        public IActionResult Delete(int id)
+        {
+            var user = context.Users2.Find(id);
+            context.Users2.Remove(user);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
--- Models/User2.cs
+++ Models/User2.cs
@@ -1,15 +1,24 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace WebApplication1.Models
 {
     public class User2 { 
-        public int Id { get; set; } 
-        public string Name { get; set; }
-        public string Email { get; set; } 
-        public int Age { get; set; } 
-        public decimal Score { get; set; } }
-
+            public int Id { get; set; }
+            [Required(ErrorMessage ="Name is required")]
+            [MinLength(3, ErrorMessage ="min length is 3")]
+            [MaxLength(20, ErrorMessage ="max length is 20")]
+       
+            public string Name { get; set; }
+            [Required]
+            [DataType(DataType.EmailAddress)]
+            public string Email { get; set; }
+            [Range(20, 80)]
+            public int Age { get; set; }
+            [Range(10, 100)]
+            public decimal Score { get; set; } 
+    }
 }
